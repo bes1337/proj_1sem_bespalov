8082c2c6cca2bf411997fddb0dc0bdbda598487b
User <bespalovartem51@gmail.com>
2023-03-17 16:55:38 +0300

    #Commit

---
+++ PZ_16/__init__.py
@@ -0,0 +1,5 @@
+from .circle.code import *
+from .square.code import *
+from .triangle.code import *
+
+NAME = "Пакеты счёта фигур
---
+++ PZ_16/circle/code.py
@@ -0,0 +1,5 @@
+def circle_perimeter(R):
+    return 2*3.14*R
+
+def circle_area(R):
+    return 3.14*R**2
